acf4b4adcb6aab56bb95a0ad5774b38ebad54e3b
booky10 <github@booky.tk>
2021-02-28 18:04:40 +0100

    added essential bukkit plugin files

---
+++ src/main/java/tk/booky/vanillatempban/VanillaTempBanMain.java
@@ -0,0 +1,13 @@
+package tk.booky.vanillatempban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanillaTempBanMain extends JavaPlugin {
+
+    public static VanillaTempBanMain main;
+
+    @Override
+    public void onEnable() {
+        main = this;
+    }
+}
